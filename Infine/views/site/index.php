8d6956ad84b352ae21359c3af080eae2e49d121b
BabuYii <infobabu.sab@gmail.com>
2014-07-02 11:31:51 +0530

    Name changes

--- Infine/views/site/index.php
+++ Infine/views/site/index.php
@@ -1,11 +1,11 @@
 <?php
 /* @var $this yii\web\View */
-$this->title = 'My Yii Application';
+$this->title = 'Infine Technologies -Open';
 ?>
 <div class="site-index">
 
     <div class="jumbotron">
-        <h1>Congratulations!</h1>
+        <h1>Welcome</h1>
 
         <p class="lead">You have successfully created your Yii-powered application.</p>
 
