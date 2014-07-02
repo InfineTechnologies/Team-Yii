8b54231b0b377dc45c475f7a2a5f5e40b8920a85
BabuYii <infobabu.sab@gmail.com>
2014-07-02 11:46:54 +0530

    cc

--- Infine/views/site/about.php
+++ Infine/views/site/about.php
@@ -9,8 +9,11 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        This is the About page. You may modify the following file to customize its content:
+        Our team has efficient and skillful team members. 
+        Individual members have thought hard about creative design and user friendly application development for the project .
+        Everyone takes initiative to get things done. Each teammate trusts the judgement of the others.
+        Everyone is supportive of the project and of others. Team decisions are made using organized, logical methods.
+        Team goals are given realistic time frames. Everyone is focused on the ultimate goal of the project,
+        while also digging into the underlying details.Everyone participates actively and positively in meetings and projects.
     </p>
-
-    <code><?= __FILE__ ?></code>
 </div>
